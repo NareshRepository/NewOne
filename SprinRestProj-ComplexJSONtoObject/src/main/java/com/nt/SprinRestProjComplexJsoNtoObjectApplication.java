3287a1c1ecb15f6d2873f120293894345160ddba
Naresh <Naresh@admin1.bbrouter>
2024-01-07 17:16:26 +0530

    commit changes

---
+++ SprinRestProj-ComplexJSONtoObject/src/main/java/com/nt/SprinRestProjComplexJsoNtoObjectApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinRestProjComplexJsoNtoObjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinRestProjComplexJsoNtoObjectApplication.class, args);
+	}
+
+}
